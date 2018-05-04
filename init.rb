a463b970827cab878d9e8c064cfb5a0c48ff5fcb
Jens Kraemer <jk@jkraemer.net>
2018-05-04 12:05:04 +0800

    require a reasonably 'fresh' version of Redmine, bump version

--- init.rb
+++ init.rb
@@ -7,9 +7,9 @@ Redmine::Plugin.register :redmine_msteams do
   url 'https://github.com/simplethings/redmine-msteams'
   author_url 'http://www.simplethings.de'
   description 'Microsoft Teams chat integration'
-  version '0.1'
+  version '0.2.0'
 
-  requires_redmine :version_or_higher => '0.8.0'
+  requires_redmine :version_or_higher => '3.3.0'
 
   settings :default => { 'display_watchers' => 'no' },
            :partial => 'settings/msteams_settings'
